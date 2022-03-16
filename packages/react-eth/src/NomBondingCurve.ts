5489e8720debbe5acf4367998172e54ccad04605
sbvhev <rohannepal200@gmail.com>
2022-03-16 16:21:26 +0100

    ExchangeQuote now uses bonding curve

--- packages/react-eth/src/NomBondingCurve.ts
+++ packages/react-eth/src/NomBondingCurve.ts
@@ -37,4 +37,73 @@ export class NomBondingCurve {
   public bNomIncreaseBondAllowance(amountAtoms: BigNumber, gasPriceAtoms: BigNumber) {
     return this.bNomIncreaseAllowance(this.bondContract.address, amountAtoms, gasPriceAtoms);
   }
+
+  public buyQuoteETH(amountETH: BigNumber): Promise<BigNumber> {
+    return this.bondContract.buyQuoteETH(amountETH.toFixed(0));
+  }
+
+  public sellQuoteNOM(amountNOM: BigNumber): Promise<BigNumber> {
+    return this.bondContract.sellQuoteNOM(amountNOM.toFixed(0));
+  }
+
+  public async buyNOM(
+    bidAmount: BigNumber,
+    askAmount: BigNumber,
+    slippage: BigNumber,
+    gasPrice: BigNumber
+  ) {
+    const gasFeeRaw = await this.bondContract.estimateGas.buyNOM(
+      askAmount.toFixed(0),
+      slippage.toFixed(0),
+      {
+        value: bidAmount.toFixed(0),
+      }
+    );
+
+    const gasFee = new BigNumber(gasFeeRaw.toString());
+    // eslint-disable-next-line no-case-declarations
+    const gas = gasFee.times(gasPrice);
+
+    if (bidAmount.lt(gas)) {
+      throw new Error('lowBid');
+    }
+
+    // eslint-disable-next-line no-case-declarations
+    const bidAmountUpdate = bidAmount.minus(gasFee.times(gasPrice));
+    // eslint-disable-next-line no-case-declarations
+    const askAmountUpdateRaw = await this.bondContract.buyQuoteETH(bidAmountUpdate.toFixed(0));
+    // eslint-disable-next-line no-case-declarations
+    const askAmountUpdate = new BigNumber(askAmountUpdateRaw.toString());
+
+    const tx: ContractTransaction = await this.bondContract.buyNOM(
+      askAmountUpdate.toFixed(0),
+      slippage.toFixed(0),
+      {
+        value: bidAmountUpdate.toFixed(0),
+        gasPrice: gasPrice.toFixed(0),
+        gasLimit: gasFee.toFixed(0),
+      }
+    );
+
+    const receipt = await tx.wait();
+    return [receipt, tx];
+  }
+
+  public async sellNOM(
+    bidAmount: BigNumber,
+    askAmount: BigNumber,
+    slippage: BigNumber,
+    gasPrice: BigNumber
+  ) {
+    const tx = await this.bondContract.sellNOM(
+      bidAmount.toFixed(0),
+      askAmount.toFixed(0),
+      slippage.toFixed(0),
+      {
+        gasPrice: gasPrice.toFixed(0),
+      }
+    );
+    const receipt = await tx.wait();
+    return [receipt, tx];
+  }
 }
